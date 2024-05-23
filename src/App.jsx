d6d6677a2fe4ec9e251f5741a8c3cad28ba524c9
강지현 <kangjihyeon@gangjihyeon-ui-MacBookAir.local>
2024-05-23 14:37:56 +0900

    ex3 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React, { useState, useEffect } from "react";
-import TextInput from "./components/TextInput";
+import React, { useState, useEffect } from 'react';
+import TextInput from './components/TextInput';
 
 function App() {
+  // const textsObj = JSON.stringify(inputValue); // 객체, 배열 -> 문자열
+  // window.localStorage.setItem('texts', '홍길동');  // 스토리지에 저장
+  // window.localStorage.getItem('texts');  // 스토리지 값 읽기
+  // const textsObj = JSON.parse(inputValue); // 문자열 -> 객체, 배열
+
   // TODO: 로컬 스토리지에서 초기 상태로 사용할 값을 가져오세요. 새로고침 해도 기존 상태를 유지하는 것이 목적입니다.
   // 로컬스토리지에 값이 없을 경우 빈배열[] 로 설정하세요.
-  const [texts, setTexts] = useState([]);
+  // const savedText = localStorage.getItem('texts')
+  // console.log(JSON.parse((savedText)) ||[]);
+  const [texts, setTexts] = useState(
+    JSON.parse(localStorage.getItem('texts')) || []
+  );
 
   useEffect(() => {
     // TODO: 상태가 변경될 때마다 로컬 스토리지에 저장. key 값은 texts 로 합시다.
+    localStorage.setItem('texts', JSON.stringify(texts));
   }, [texts]);
 
   const onAddText = (text) => {
